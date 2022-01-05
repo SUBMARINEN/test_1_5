82e3e9e4c14789cfe00c3326f945ccea273309de
c学习 <97146951+SUBMARINEN@users.noreply.github.com>
2022-01-05 14:09:15 +0800

    Add files via upload

---
+++ test_9_16/test_9_16/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	printf("%c\n",'\x61');
+//	//printf("c:\\test\\32\\test.c");
+//	//printf("%s\n","\"");
+//	//printf("abc\n");
+//	//char arr1[]="abc";
+//	//char arr2[] = { 'a', 'b', 'c' };
+//	//printf("%d\n", strlen(arr1));
+//	//printf("%d\n", strlen(arr2));
+//	//printf("%s\n",arr1);
+//	//printf("%s\n",a
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？(1/0)>:");
+//	scanf("%d",&input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("敲一行代码 %d\n",line);
+//		line++;
+//	}
+//	if (line>20000)
+//	printf("好offer\n");
+//	return 0;
+//}
+//int main()
+//{
+//	/*int num1 = 10;
+//	int num2 = 20;
+//	int sum = 0;*/
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	printf("%d\n", arr[4]);
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a^b;
+	printf("%d\n",c);
+	
+	return 0;
+}
+
+
